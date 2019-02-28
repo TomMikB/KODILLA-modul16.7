0dc613107de8a3a1e76e50488c8180241d082b2a
Tomasz Biernacki <tmbiernacki@szczecin.home.pl>
2019-02-28 18:43:40 +0100

    Modify js files

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-		str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-}
+'use strict';
 
+// template generator
 function generateTemplate(name, data, basicElement) {
 	var template = document.getElementById(name).innerHTML;
 	var element = document.createElement(basicElement || 'div');
@@ -16,20 +9,31 @@ function generateTemplate(name, data, basicElement) {
 	return element;
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+var baseUrl = 'https://cors-anywhere.herokuapp.com/' + 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '3596',
+	'X-Auth-Token': 'cff14b2f5b96109f01f247bb02c5ae1b'
+};
 
-// DODAWANIE KOLUMN DO TABLICY
-board.addColumn(todoColumn);
-board.addColumn(doingColumn);
-board.addColumn(doneColumn);
+fetch(baseUrl + '/board', { headers: myHeaders })
+	.then(function(resp) {
+		return resp.json();
+	})
+	.then(function(resp) {
+		setupColumns(resp.columns);
+	});
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.addColumn(col);
+		setupCards(col, column.cards);
+	});
+}
 
-// DODAWANIE KART DO KOLUMN
-todoColumn.addCard(card1);
-doingColumn.addCard(card2);
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var cardObj = new Card(card.id, card.name);
+		col.addCard(cardObj);
+	});
+}
